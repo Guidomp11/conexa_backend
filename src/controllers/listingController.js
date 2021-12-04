89b2ea007e80980122b648d877627d61bf986581
Guido Ponce <ponceguido@gmail.com>
2021-12-04 13:35:50 -0300

    listPhotos function controller added

--- src/controllers/listingController.js
+++ src/controllers/listingController.js
@@ -2,7 +2,22 @@ const fetch = require('node-fetch');
 
 module.exports = {
     listPhotos: async (req, res) => {
-        
+        try{
+            const { offset, limit } = req.query;
+
+            if(!offset || !limit) throw {status: 400, msg: `Offset: ${offset} - Limit: ${limit}`};
+
+            const photos = await fetch(`http://jsonplaceholder.typicode.com/photos?_start=${offset}&_limit=${limit}`);
+            
+            const jsonPhotos = await photos.json();
+            
+            return res.status(200).json({response: jsonPhotos});
+
+        }catch(e){
+            const { status, errors } = e;
+
+            return res.status(status || 400).json({errors});
+        }
     },
     listPosts: async (req, res) => {
         try{
