fcae56db760d508eb296b31f965d6f847e70ad7a
Guido Ponce <ponceguido@gmail.com>
2021-12-04 13:35:29 -0300

    listPosts function on controller added

--- src/controllers/listingController.js
+++ src/controllers/listingController.js
@@ -1,8 +1,18 @@
+const fetch = require('node-fetch');
+
 module.exports = {
     listPhotos: async (req, res) => {
         
     },
     listPosts: async (req, res) => {
-        
+        try{
+            const posts = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+            const jsonPosts = await posts.json();
+
+            return res.status(200).json({response: jsonPosts});
+        }catch(e){
+            return res.status(400).json({e});
+        }
     }
 };
